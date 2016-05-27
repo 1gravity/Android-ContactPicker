6f3de53823f0752834b8f7fb8f08fd3a8c8d73f6
Emanuel Moecklin <contact@1gravity.com>
2016-05-27 12:13:11 -0400

    Added theming to the demo app

--- demo/src/main/java/com/onegravity/contactpicker/demo/DemoActivity.java
+++ demo/src/main/java/com/onegravity/contactpicker/demo/DemoActivity.java
@@ -23,6 +23,8 @@ import android.provider.ContactsContract;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.BulletSpan;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -33,15 +35,35 @@ import com.onegravity.contactpicker.contact.ContactSortOrder;
 import com.onegravity.contactpicker.core.ContactPickerActivity;
 import com.onegravity.contactpicker.picture.ContactPictureType;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class DemoActivity extends BaseActivity {
 
+    private static final String EXTRA_LIGHT_THEME = "EXTRA_LIGHT_THEME";
+    private static final String EXTRA_CONTACTS = "EXTRA_CONTACTS";
+
     private static final int REQUEST_CONTACT = 0;
+
+    private boolean mUseLightTheme;
+    private List<Contact> mContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // read parameters either from the Intent or from the Bundle
+        if (savedInstanceState != null) {
+            mUseLightTheme = savedInstanceState.getBoolean("mUseLightTheme");
+            mContacts = (List<Contact>) savedInstanceState.getSerializable("mContacts");
+        }
+        else {
+            Intent intent = getIntent();
+            mUseLightTheme = intent.getBooleanExtra(EXTRA_LIGHT_THEME, false);
+            mContacts = (List<Contact>) intent.getSerializableExtra(EXTRA_CONTACTS);
+        }
+
+        setTheme(mUseLightTheme ? R.style.Theme_Light : R.style.Theme_Dark);
 
         // set layout
         setContentView(R.layout.main);
@@ -68,6 +90,19 @@ public class DemoActivity extends BaseActivity {
         else {
             finish();
         }
+
+        // populate contact list
+        populateContactList(mContacts);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean("mUseLightTheme", mUseLightTheme);
+        if (mContacts != null) {
+            outState.putSerializable("mContacts", (Serializable) mContacts);
+        }
     }
 
     @Override
@@ -76,24 +111,66 @@ public class DemoActivity extends BaseActivity {
             data != null && data.hasExtra(ContactPickerActivity.RESULT_CONTACT_DATA)) {
 
             // we got a result from the contact picker --> show the picked contacts
-            TextView contactsView = (TextView) findViewById(R.id.contacts);
-            SpannableStringBuilder result = new SpannableStringBuilder();
-            try {
-                List<Contact> contacts = (List<Contact>) data.getSerializableExtra(ContactPickerActivity.RESULT_CONTACT_DATA);
-                int pos = 0;
-                for (Contact contact : contacts) {
-                    String displayName = contact.getDisplayName();
-                    result.append(displayName + "\n");
-                    result.setSpan(new BulletSpan(15), pos, pos + displayName.length() + 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                    pos += displayName.length() + 1;
-                }
-            }
-            catch (Exception e) {
-                result.append(e.getMessage());
-            }
+            mContacts = (List<Contact>) data.getSerializableExtra(ContactPickerActivity.RESULT_CONTACT_DATA);
+            populateContactList(mContacts);
 
-            contactsView.setText(result);
         }
+    }
+
+    private void populateContactList(List<Contact> contacts) {
+        if (contacts == null || mContacts.isEmpty()) return;
+
+        // we got a result from the contact picker --> show the picked contacts
+        TextView contactsView = (TextView) findViewById(R.id.contacts);
+        SpannableStringBuilder result = new SpannableStringBuilder();
+        try {
+            int pos = 0;
+            for (Contact contact : contacts) {
+                String displayName = contact.getDisplayName();
+                result.append(displayName + "\n");
+                result.setSpan(new BulletSpan(15), pos, pos + displayName.length() + 1, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                pos += displayName.length() + 1;
+            }
+        }
+        catch (Exception e) {
+            result.append(e.getMessage());
+        }
+
+        contactsView.setText(result);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.contact_picker_demo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        int textId = mUseLightTheme ? R.string.dark_theme : R.string.light_theme;
+        menu.findItem(R.id.action_theme).setTitle(textId);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_theme) {
+            mUseLightTheme = ! mUseLightTheme;
+            Intent intent = new Intent(this, this.getClass())
+                    .putExtra(EXTRA_LIGHT_THEME, mUseLightTheme);
+            if (mContacts != null) {
+                intent.putExtra(EXTRA_CONTACTS, (Serializable) mContacts);
+            }
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return false;
     }
 
 }
